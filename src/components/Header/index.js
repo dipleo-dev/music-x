d8f3d97d716d52b6e210059249cbd317a8086f35
[dipleo-dev] <dipleo.dev@gmail.com>
2021-12-03 02:50:00 +0100

    Folder structure and Header Create

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import { logo } from "../../assets";
+const Header = (props) => {
+  return (
+    <header className="header flex justify-sb">
+      <div className="logo">
+        <img src={logo} alt="M" width="40" height="40" />
+      </div>
+      <h2>Enjoy your Music</h2>
+    </header>
+  );
+};
+
+export default Header;
