2580e09721c705674f97c9ea6304ebe0bbe45454
[dipleo-dev] <dipleo.dev@gmail.com>
2021-12-08 05:43:32 +0100

    feat(Drawer,AudioPlayer) next and prev button , track progress indicator

--- src/components/Drawer/AudioPlayer.js
+++ src/components/Drawer/AudioPlayer.js
@@ -1,6 +1,19 @@
 import { PrevIcon, NextIcon, PlayIcon, pauseBlackIcon } from "../../assets";
 
-const AudioPlayer = ({ name, cover_image_path, isPlaying }) => {
+const AudioPlayer = ({
+  name,
+  cover_image_path,
+  isPlaying,
+  duration,
+  trackProgress,
+  onChangeTrackProgress,
+  onPlayPause,
+  prevTrack,
+  nextTrack,
+}) => {
+  const currentProgress = (trackProgress / duration) * 100;
+  const trackProgressStyling = `linear-gradient(to right, #9393ff ${currentProgress}%, #ffffff ${currentProgress}%)`;
+
   return (
     <div className="audio-player-lg">
       <div className="audio-img-lg">
@@ -15,23 +28,28 @@ const AudioPlayer = ({ name, cover_image_path, isPlaying }) => {
       </div>
 
       <div className="audio-player-progress">
-        <input type="range" min={"0"} max={"100"} />
+        <input
+          type="range"
+          min={"0"}
+          step={"1"}
+          max={duration ? duration : 0}
+          value={trackProgress}
+          onChange={onChangeTrackProgress}
+          style={{ background: trackProgressStyling }}
+        />
       </div>
       <div className="audio-controls flex justify-sb">
-        <button className="prev-btn">
+        <button onClick={prevTrack} className="prev-btn">
           <img src={PrevIcon} alt="prev" />
         </button>
-        <button
-          // onClick={() => setIsPlaying(!isPlaying)}
-          className="play-pause-btn"
-        >
+        <button onClick={onPlayPause} className="play-pause-btn">
           {isPlaying ? (
             <img src={pauseBlackIcon} alt="pause" />
           ) : (
             <img src={PlayIcon} alt="play" />
           )}
         </button>
-        <button className="next-btn">
+        <button onClick={nextTrack} className="next-btn">
           <img src={NextIcon} alt="next" />
         </button>
       </div>
--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -7,35 +7,73 @@ const Drawer = ({ trackIndex, audioList }) => {
   const [slideUp, setSlideUp] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTrackIndex, setCurrentTrackIndex] = useState(trackIndex);
+  const [trackProgress, setTrackProgress] = useState("");
 
   const {
     name = "",
     cover_image_path = "",
     music_file_path = "",
-  } = trackIndex !== -1 ? audioList[trackIndex] : {};
+  } = currentTrackIndex !== -1 ? audioList[currentTrackIndex] : {};
 
   const audioSrc = `${music_file_path}`;
   const audioRef = useRef(new Audio(audioSrc));
   const intervalRef = useRef();
-  console.log(audioSrc, "audiosrc");
+
+  const startTimer = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setTrackProgress(audioRef.current.currentTime);
+    }, 1000);
+  };
+
+  const onChangeTrackProgress = (e) => {
+    setTrackProgress(e.target.value);
+    audioRef.current.currentTime = e.target.value;
+  };
+
+  const nextTrack = () => {
+    if (currentTrackIndex < audioList.length - 1) {
+      setCurrentTrackIndex((prevIndex) => prevIndex + 1);
+      setTrackProgress(0);
+    } else {
+      setCurrentTrackIndex(0);
+    }
+  };
+
+  const prevTrack = () => {
+    if (currentTrackIndex) {
+      setCurrentTrackIndex((prevIndex) => prevIndex - 1);
+    } else {
+      setCurrentTrackIndex(audioList.length - 1);
+    }
+  };
+  useEffect(() => {
+    console.log(audioSrc, "audiosrc");
+    clearInterval(intervalRef.current);
+    setCurrentTrackIndex(trackIndex);
+  }, [trackIndex]);
 
   useEffect(() => {
-    audioRef.current.pause();
-
-    audioRef.current = new Audio(audioSrc);
-
-    audioRef.current.play();
-
-    setIsPlaying(true);
-
-    setCurrentTrackIndex(trackIndex);
-  }, [trackIndex, audioSrc]);
-
+    if (currentTrackIndex !== -1) {
+      // if a music is already playing then we will stop it and assign currently selected one
+      audioRef.current.pause();
+      // new audio initialize
+      audioRef.current = new Audio(audioSrc);
+      //playing initialize audio
+      audioRef.current.play();
+      // set isPlaying true when music started playing
+      setIsPlaying(true);
+      // start progress of the audio
+      startTimer();
+    }
+  }, [currentTrackIndex]);
   useEffect(() => {
     if (isPlaying) {
       audioRef.current.play();
+      startTimer();
     } else {
       audioRef.current.pause();
+      clearInterval(intervalRef.current);
     }
   }, [isPlaying]);
 
@@ -57,6 +95,12 @@ const Drawer = ({ trackIndex, audioList }) => {
           name={name}
           cover_image_path={cover_image_path}
           isPlaying={isPlaying}
+          duration={audioRef.current.duration}
+          trackProgress={trackProgress}
+          onChangeTrackProgress={onChangeTrackProgress}
+          onPlayPause={() => setIsPlaying(!isPlaying)}
+          nextTrack={nextTrack}
+          prevTrack={prevTrack}
         />
       )}
       {!slideUp && (
